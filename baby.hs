8be322bd689f15eb3ed9c3976df48d73806c0945
Seth Guberman <sguberman@gmail.com>
2018-01-26 15:18:30 -0500

    complete ch2 starting out

---
+++ baby.hs
@@ -0,0 +1,11 @@
+doubleMe x = x + x
+doubleUs x y = doubleMe x + doubleMe y
+doubleSmallNumber x = if x > 100
+			then x
+			else x*2
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+conanO'Brien = "It's a-me, Conan O'Brien!"
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x ]
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z'] ]
+rightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2 ]
+rightTriangles' = [ (a,b,c) | (a,b,c) <- rightTriangles, a+b+c == 24 ]
