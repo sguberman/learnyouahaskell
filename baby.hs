4a5b38a62cd77007a9e2a298ae08c0022cc382db
Seth Guberman <sguberman@gmail.com>
2018-01-27 16:27:53 -0500

    complete ch3 types and type classes

--- baby.hs
+++ baby.hs
@@ -1,11 +1,16 @@
 doubleMe x = x + x
 doubleUs x y = doubleMe x + doubleMe y
-doubleSmallNumber x = if x > 100
-			then x
-			else x*2
+doubleSmallNumber x = if x > 100 then x else x*2
 doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
 conanO'Brien = "It's a-me, Conan O'Brien!"
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x ]
+removeNonUppercase :: String -> String
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z'] ]
 rightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2 ]
 rightTriangles' = [ (a,b,c) | (a,b,c) <- rightTriangles, a+b+c == 24 ]
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
