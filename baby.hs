27badd36e8a25dacba26e4a096e437e46bd5105e
Seth Guberman <sguberman@gmail.com>
2018-01-27 19:52:18 -0500

    start ch4 syntax in functions

--- baby.hs
+++ baby.hs
@@ -3,11 +3,13 @@ doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100 then x else x*2
 doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
 conanO'Brien = "It's a-me, Conan O'Brien!"
+
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x ]
 removeNonUppercase :: String -> String
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z'] ]
 rightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2 ]
 rightTriangles' = [ (a,b,c) | (a,b,c) <- rightTriangles, a+b+c == 24 ]
+
 factorial :: Integer -> Integer
 factorial n = product [1..n]
 circumference :: Float -> Float
