731d51161c0f97d0d5ed93e74b7783d536a0a99b
Seth Guberman <sguberman@gmail.com>
2018-01-28 11:36:42 -0500

    continue ch3 syntax in functions

--- syntax.hs
+++ syntax.hs
@@ -16,4 +16,18 @@ addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 head' :: [a] -> a
 head' [] = error "Can't call head on an empty list, dummy!"
 head' (x:_) = x
+tell :: (Show a) => [a] -> String
+tell [] = "The list is empty"
+tell (x:[]) = "The list has one element: " ++ show x
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "The list is long. The first two elements are: " ++ show x ++ " and " ++ show y
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (_:xs) = 1 + length' xs
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
+capital :: String -> String
+capital "" = "Empty string, whoops!"
+capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
 
