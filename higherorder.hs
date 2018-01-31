c81edc2e5a13cb770da91909934af3bc3be7193e
Seth Guberman <sguberman@gmail.com>
2018-01-31 15:48:04 -0500

    continue ch6 higher order functions

--- higherorder.hs
+++ higherorder.hs
@@ -43,3 +43,27 @@ numLongChains = length (filter (\xs -> length xs > 15) (map chain [1..100]))
 sum' :: (Num a) => [a] -> a
 sum' = foldl (+) 0
 
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' y ys = foldl (\acc x -> if x == y then True else acc) False ys
+
+map2' :: (a -> b) -> [a] -> [b]
+map2' f xs = foldr (\x acc -> f x : acc) [] xs
+
+maximum' :: (Ord a) => [a] -> a
+maximum' = foldr1 (\x acc -> if x > acc then x else acc)
+
+reverse' :: [a] -> [a]
+reverse' = foldl (\acc x -> x : acc) []
+
+product' :: (Num a) => [a] -> a
+product' = foldr1 (*)
+
+filter2' :: (a -> Bool) -> [a] -> [a]
+filter2' p = foldr (\x acc -> if p x then x : acc else acc) []
+
+head' :: [a] -> a
+head' = foldr1 (\x _ -> x)
+
+last' :: [a] -> a
+last' = foldl1 (\_ x -> x)
+
